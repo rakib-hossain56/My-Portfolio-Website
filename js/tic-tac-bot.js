e4f4cdbd662f196c881bc951627fb9e92e1656aa
Rakib Hossain <79511055+rakib-hossain56@users.noreply.github.com>
2022-11-24 23:52:40 +0600

    add file
    adding js file

---
+++ js/tic-tac-bot.js
@@ -0,0 +1,281 @@
+//selecting all required elements
+
+const selectBox = document.querySelector(".select-box"),
+//selectXBtn = selectBox.querySelector(".playerX"),
+//selectOBtn = selectBox.querySelector(".playerO"),
+playBoard = document.querySelector(".play-board"),
+allBox = document.querySelectorAll("section span"),
+players = document.querySelector(".players"),
+resultBox = document.querySelector(".result-box"),
+wonText = document.querySelector(".won-text"),
+replayBtn = document.querySelector(".btn");
+let runBot = true;
+
+window.onload = ()=>{ //once window loaded
+    let randomNumber = Math.floor(Math.random() * 10);
+    
+    for(let i = 0; i < allBox.length; i++){ //add onclick attribute in all available section's spans
+        allBox[i].setAttribute("onclick", "clickedBox(this)");
+    }
+
+    players.setAttribute("class", "players active player"); //adding three class names in player element
+   
+
+    if(randomNumber > 5){
+        bot(runBot);
+    }
+
+   //  selectXBtn.onclick = () =>{
+
+   //      selectBox.classList.add("hide"); //hide the select box on playerX button clicked
+   //      playBoard.classList.add("show"); //show the playboard section on playerX button clicked
+   //  }
+   //  selectOBtn.onclick = () =>{
+   //      selectBox.classList.add("hide"); //hide the select box on playerO button clicked
+   //      playBoard.classList.add("show"); //show the playboard section on playerO button clicked
+   //      //players.setAttribute("class", "players active player"); //adding three class names in player element
+   //  }
+
+}
+
+let playerXIcon = "fas fa-times"; //class name of fontawesome cross icon
+let playerOIcon = "far fa-circle"; //class name of fontawesome circle icon
+let playerSign = "Bot"; // suppose player will be x
+
+
+
+function clickedBox(element){
+    if(players.classList.contains("player")){ //if players element has containts .player
+       
+        element.innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element
+        players.classList.remove("active");
+        playerSign = "O"; // if player will be o then we'll change the sign
+        element.setAttribute("id", playerSign); // adding player's id for both x & o
+    }else{
+        element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element
+        players.classList.add("active");
+        element.setAttribute("id", playerSign);
+    }
+    selectWinner(); // calling the winner function
+    playBoard.style.pointerEvents = "none"; //once user select then user can't select any other box until bot select
+    element.style.pointerEvents = "none"; //once user select any box then that box can't be selected again
+
+    let randomDelayTime = ((Math.random() * 1000) + 200).toFixed(); //generating random time delay so bot will delay randomly to select box
+    setTimeout(()=>{
+        bot(runBot); // calling box function
+    }, randomDelayTime); //passing random delay time
+    
+    
+}
+
+// bot click funtion
+
+function bot(runBot){
+    let playerId = playerSign;
+
+   if(runBot){ //if runbot is true then run the following codes
+     //first change the playerSign...so if user has X value in id then bot will have o
+     playerSign = "O";
+    let botId = playerSign;
+
+    // declering nesseary arrays 
+     let array = [0, 2, 6, 8];
+     let array2 = [];
+     let array3 = [];
+     let one = allBox[0].getAttribute("id") == playerId;
+     let two = allBox[1].getAttribute("id") == playerId;
+     let three = allBox[2].getAttribute("id") == playerId;
+     let four = allBox[3].getAttribute("id") == playerId;
+     let five = allBox[4].getAttribute("id") == playerId;
+     let six = allBox[5].getAttribute("id") == playerId;
+     let seven = allBox[6].getAttribute("id") == playerId;
+     let eight = allBox[7].getAttribute("id") == playerId;
+     let nine = allBox[8].getAttribute("id") == playerId;
+
+     // array for bot
+     let botTone = allBox[0].getAttribute("id") == botId;
+     let botTtwo = allBox[1].getAttribute("id") == botId;
+     let botTthree = allBox[2].getAttribute("id") == botId;
+     let botTfour = allBox[3].getAttribute("id") == botId;
+     let botTfive = allBox[4].getAttribute("id") == botId;
+     let botTsix = allBox[5].getAttribute("id") == botId;
+     let botTseven = allBox[6].getAttribute("id") == botId;
+     let botTeight = allBox[7].getAttribute("id") == botId;
+     let botTnine = allBox[8].getAttribute("id") == botId;
+
+     for(let i = 0; i < allBox.length; i++){
+        
+        if(allBox[i].childElementCount == 0){ //if span has no child element
+            array3.push(i); //inserting unclicked or unselected boxes inside array means that span has no children
+            // console.log(i + " " + "has no children");
+        }
+    }
+
+     for(let i of array){
+        
+        if(allBox[i].childElementCount == 0){ //if span has no child element
+            array2.push(i); //inserting unclicked or unselected boxes inside array means that span has no children
+            // console.log(i + " " + "has no children");
+        }
+    }
+// making condition for bot selection
+    // case [1,2,3]
+     if(((botTone && botTtwo) || (one && two)) && allBox[2].childElementCount == 0){
+        randomBox = 2;
+     }
+     else if(((botTtwo && botTthree) || (two && three)) && allBox[0].childElementCount == 0){
+        randomBox = 0;
+     }
+     else if(((botTone && botTthree) || (one && three)) && allBox[1].childElementCount == 0){
+        randomBox = 1;
+     }// case [4,5,6]
+     else if(((botTfour && botTfive) || (four && five)) && allBox[5].childElementCount == 0){
+        randomBox = 5;
+     }
+     else if(((botTfive && botTsix) || (five && six)) && allBox[3].childElementCount == 0){
+        randomBox = 3;
+     }
+     else if(((botTfour && botTsix) || (four && six)) && allBox[4].childElementCount == 0){
+        randomBox = 4;
+     }// case [7,8,9]
+     else if(((botTseven && botTeight) || (seven && eight)) && allBox[8].childElementCount == 0){
+        randomBox = 8;
+     }
+     else if(((botTeight && botTnine) || (eight && nine)) && allBox[6].childElementCount == 0){
+        randomBox = 6;
+     }
+     else if(((botTseven && botTnine) || (seven && nine)) && allBox[7].childElementCount == 0){
+        randomBox = 7;
+     }// case [1,4,7]
+     else if(((botTone && botTfour) || (one && four)) && allBox[6].childElementCount == 0){
+        randomBox = 6;
+     }
+     else if(((botTfour && botTseven) || (four && seven)) && allBox[0].childElementCount == 0){
+        randomBox = 0;
+     }
+     else if(((botTone && botTseven) || (one && seven)) && allBox[3].childElementCount == 0){
+        randomBox = 3;
+     }// case [2,5,8]
+     else if(((botTtwo && botTfive) || (two && five)) && allBox[7].childElementCount == 0){
+        randomBox = 7;
+     }
+     else if(((botTfive && botTeight) || (five && eight)) && allBox[1].childElementCount == 0){
+        randomBox = 1;
+     }
+     else if(((botTtwo && botTeight) || (two && eight)) && allBox[4].childElementCount == 0){
+        randomBox = 4;
+     }// case [3,6,9]
+     else if(((botTthree && botTsix) || (three && six)) && allBox[8].childElementCount == 0){
+        randomBox = 8;
+     }
+     else if(((botTsix && botTnine) || (six && nine)) && allBox[2].childElementCount == 0){
+        randomBox = 2;
+     }
+     else if(((botTthree && botTnine) || (three && nine)) && allBox[5].childElementCount == 0){
+        randomBox = 5;
+     }// case [1,5,9]
+     else if(((botTone && botTfive) || (one && five)) && allBox[8].childElementCount == 0){
+        randomBox = 8;
+     }
+     else if(((botTfive && botTnine) || (five && nine)) && allBox[0].childElementCount == 0){
+        randomBox = 0;
+     }
+     else if(((botTone && botTnine) || (one && nine)) && allBox[4].childElementCount == 0){
+        randomBox = 4;
+     }// case [3,5,7]
+     else if(((botTthree && botTfive) || (three && five)) && allBox[6].childElementCount == 0){
+        randomBox = 6;
+     }
+     else if(((botTfive && botTseven) || (five && seven)) && allBox[2].childElementCount == 0){
+        randomBox = 2;
+     }
+     else if(((botTthree && botTseven) || (three && seven)) && allBox[4].childElementCount == 0){
+        randomBox = 4;
+     }
+     else{
+      console.log("else")
+        array2.length == 0? randomBox = array3[Math.floor(Math.random() * array3.length)] : randomBox = array2[Math.floor(Math.random() * array2.length)] //getting random index from array so bot will select random unselected box
+        
+        
+    }
+console.log(randomBox)
+     
+     if(array.length > 0){
+         if(players.classList.contains("player")){ //if players element has containts .player
+             allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element
+             players.classList.add("active");
+             
+             //if user is o then the box id value will be x
+            playerSign = "Bot";
+             allBox[randomBox].setAttribute("id", playerSign);
+         }else{
+             allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element
+             players.classList.remove("active");
+            
+             allBox[randomBox].setAttribute("id", playerSign);
+         }
+         selectWinner(); // calling the winner function
+     }
+ 
+     allBox[randomBox].style.pointerEvents = "none"; //once bot select any box then user can't select or click on that box
+     playBoard.style.pointerEvents = "auto";
+     playerSign = "Bot"; // passing the x value
+     
+   }
+}
+
+
+//let work on select the winner
+
+function getId(classname){
+    return document.querySelector(".box" + classname).id; //returning id name
+}
+
+
+function checkId(val1, val2, val3, sign){
+   
+    if(getId(val1) == sign && getId(val2) == sign && getId(val3) == sign){
+        return true;
+    }
+}
+
+
+function selectWinner(){ //if one combination of them mathched then select the winner;
+    
+    if(checkId(1,2,3, playerSign) || checkId(4,5,6, playerSign) || checkId(7,8,9, playerSign) || checkId(1,4,7, playerSign) || checkId(2,5,8, playerSign) || checkId(3,6,9, playerSign) || checkId(1,5,9, playerSign) || checkId(3,5,7, playerSign)){
+
+        //once match won by somone then stop the bot
+
+        runBot = false;
+        bot(runBot);
+
+        setTimeout(()=>{ //we'll delay to show result box
+           playBoard.classList.remove("show");
+           resultBox.classList.add("show");
+           
+        }, 700) // 700 ms delay
+        
+        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`;
+    }else{
+        //if match has drawn
+        // first we'll check all id...if all span has id and no one won the game then we'll draw the game
+        
+        if(getId(1) != "" && getId(2) != "" && getId(3) != "" && getId(4) != "" && getId(5) != "" && getId(6) != "" && getId(7) != "" && getId(8) != "" && getId(9) != ""){
+            runBot = false;
+            bot(runBot);
+
+            setTimeout(()=>{ //we'll delay to show result box
+                playBoard.classList.remove("show");
+                resultBox.classList.add("show");
+            
+            }, 700) // 700 ms delay
+            
+            wonText.textContent = "Match has been drawn (:-";
+        }
+    }
+}
+
+replayBtn.onclick =  () =>{
+    window.location.reload(); // reload the current page
+}
+
